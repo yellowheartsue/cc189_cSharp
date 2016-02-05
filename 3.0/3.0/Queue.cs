d29602e8f38bda3769c042ef2281c36d3f0cdbde
Yi Su <ysu@cise.ufl.edu>
2016-02-05 11:14:15 -0500

    implemented stack and queue

---
+++ 3.0/3.0/Queue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3._0
+{
+    class Queue<T>
+    {
+        private class QueueNode<T> {
+            internal T data;
+            internal QueueNode<T> next;
+
+            public QueueNode(T data) {
+                this.data = data;
+            }
+        }
+
+        private QueueNode<T> first;
+        private QueueNode<T> last;
+
+        public void add(T item) {
+            QueueNode<T> node = new QueueNode<T>(item);
+
+            if (first != null)
+                first.next = node;
+            else
+                first = node;
+
+            last = node;
+        }
+
+        public T remove() {
+            if (first == null)
+                throw new NullReferenceException();
+
+            T item = first.data;
+            first = first.next;
+            if (first == null)
+                last = null;
+
+            return item;
+        }
+
+        public T peek() {
+            if (first != null)
+                return first.data;
+
+            throw new NullReferenceException();
+        }
+
+        public bool isEmpty() {
+            return first == null;
+        }
+    }
+}
---
+++ 3.0/3.0/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3._0
+{
+    public class Stack<T>
+    {
+        private class StackNode<T> {
+            internal T data;
+            internal StackNode<T> next;
+
+            public StackNode(T data){
+                this.data = data;
+            }
+        }
+
+        private StackNode<T> top;
+
+        public T pop() {
+            if (top != null)
+                return top.data;
+            else
+                throw new NullReferenceException();
+        }
+
+        public void push(T item) {
+            StackNode<T> node = new StackNode<T>(item);
+            if (top != null)
+                node.next = top;
+
+            top = node;
+        }
+
+        public T peek() {
+            if (top != null)
+                return top.data;
+            throw new NullReferenceException();
+        }
+
+        public bool ifEmpty() {
+            return top == null;
+        }
+    }
+}
